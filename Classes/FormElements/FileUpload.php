96d588c40ee37346e778e55511040a434f769188
Abakus\SM <sm@x65.de>
2019-03-13 11:20:47 +0100

    Improved FileUpload

--- Classes/FormElements/FileUpload.php
+++ Classes/FormElements/FileUpload.php
@@ -7,6 +7,11 @@ use Neos\Form\Core\Model\FormElementInterface;
 use Neos\Form\Core\Runtime\FormRuntime;
 use Neos\Flow\ResourceManagement\PersistentResource;
 use Neos\Form\FormElements\FileUpload as NeosFileUpload;
+use Neos\Form\Validation\FileTypeValidator;
+use Neos\Flow\Validation\Validator\NotEmptyValidator;
+use \Neos\Flow\Property\PropertyMappingConfiguration;
+use Neos\Flow\Property\TypeConverter\TypedArrayConverter;
+use Neos\Error\Messages\Error;
 
 class FileUpload extends NeosFileUpload {
     /**
@@ -14,6 +19,22 @@ class FileUpload extends NeosFileUpload {
      * @Flow\Inject
      */
     protected $propertyMapper;
+    /**
+     * @return void
+     */
+    public function initializeFormElement() {
+        /*$formDefinition = $this->getRootForm();
+        $processingRule = $formDefinition->getProcessingRule($this->identifier);
+        $mappingConfig = $processingRule->getPropertyMappingConfiguration();
+
+        $typeConverter = new TypedArrayConverter();
+
+        $mappingConfig->setTypeConverter($typeConverter);
+        $this->setDataType('array<Neos\Flow\ResourceManagement\PersistentResource>');*/
+
+        //$this->setDataType('array');
+        $this->setDataType(PersistentResource::class);
+    }
 
     /**
      *
@@ -29,18 +50,46 @@ class FileUpload extends NeosFileUpload {
 
         $request = $formRuntime->getRequest();
         $arg = $this->identifier;
+
+        $formDefinition = $this->getRootForm();
+
+        $processingRule = $formDefinition->getProcessingRule($arg);
+
         if(!$request->hasArgument($arg)) {
+            if(isset($this->properties['minFiles']) && $this->properties['minFiles'] > 0) {
+                $processingRule->getProcessingMessages()->addError(new Error("Please upload at least %s files.", 1552470144, [$this->properties['minFiles']]));
+            }
             return;
         }
 
+        $fileTypeValidator = new FileTypeValidator(array('allowedExtensions' => $this->properties['allowedExtensions']));
+        $processingRule->addValidator($fileTypeValidator);
+
+        //$notEmptyValidator = new NotEmptyValidator();
+        //$processingRule->addValidator($notEmptyValidator);
+
         $c = 0;
         foreach($request->getArgument($arg) as $fileInfo) {
-            //echo 'file:<br>';
-            //var_dump($fileInfo);
-            $resource = $this->propertyMapper->convert($fileInfo, PersistentResource::class);
-            //var_dump($resource);
-            $formRuntime[$arg . '__' . $c] = $resource;
+            //$resource = $this->propertyMapper->convert($fileInfo, PersistentResource::class);
+            $resource = $processingRule->process($fileInfo);
+
+            /*if($c == 0) {
+                $formRuntime[$arg] = $resource;
+            }*/
+            $formRuntime[$arg] = '1';
+
+            $fileIdentifier = $arg . '__' . $c;
+
+            $formRuntime[$fileIdentifier] = $resource;
+
             $c++;
+        }
+
+        if(isset($this->properties['minFiles']) && $c < $this->properties['minFiles']) {
+            //throw new \ErrorException("Please upload at least %s files.", 1552470144, [$this->properties['minFiles']]);
+            $processingRule->getProcessingMessages()->addError(new Error("Please upload at least %s files.", 1552470144, [$this->properties['minFiles']]));
+        } else if(isset($this->properties['maxFiles']) && $c > $this->properties['maxFiles']) {
+            $processingRule->getProcessingMessages()->addError(new Error("Please upload a maximum of %s files.", 1552470179, [$this->properties['maxFiles']]));
         }
     }
 }
